4cd6b4f457b7e30ecd5c74e875162d10ba5b6345
Isabel Valdes <isabel.valdes@improving.com>
2022-05-26 15:29:22 -0500

    build: vanilla js button

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack"); // only add this if you don't have yet
+const { ModuleFederationPlugin } = webpack.container;
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  output: {
+    publicPath: "auto",
+    scriptType: "text/javascript",
+  },
+  mode: "development",
+  devServer: {
+    port: 3002,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "jsButton",
+      filename: "remoteEntry.js",
+      library: { type: "var", name: "jsButton" },
+      exposes: { "./JsButton": "./src/index.js" },
+      shared: {
+        ...deps,
+        "windowed-observable": {
+          singleton: true,
+          strictVersion: true,
+          eager:true,
+        }
+      }
+    }),
+  ],
+  resolve: {
+    extensions: [".js"],
+  },
+};
